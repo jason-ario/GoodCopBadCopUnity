5c36a323f8ff41ee2fac4c488f5803a4eec57a1c
jason-ario <nosajtevol@gmail.com>
2025-11-20 19:00:42 +0400

    ADD - better coding for doors

--- Assets/_GoodCopBadCop/_Scripts/Interactables/DoorController.cs
+++ Assets/_GoodCopBadCop/_Scripts/Interactables/DoorController.cs
@@ -7,20 +7,39 @@ public class DoorController : MonoBehaviour, IInteractable
     
     public void Interact(PlayerInteractionController player)
     {
-        ToggleDoor();
+        ToggleDoor(player);
     }
 
-    void ToggleDoor()
+    void ToggleDoor(PlayerInteractionController player)
     {
         if (doorOpen)
         {
             doorOpen = false;
-            _animator.SetBool("Opened", false);
+            _animator.SetBool("OpenedIn", false);
+            _animator.SetBool("OpenedOut", false);
         }
         else
         {
             doorOpen = true;
-            _animator.SetBool("Opened", true);
+            
+            // Calculate if player is in front or behind the door
+            Vector3 doorForward = transform.forward;
+            Vector3 playerToDoor = transform.position - player.transform.position;
+            
+            // Dot product tells us which side the player is on
+            float side = Vector3.Dot(doorForward, playerToDoor);
+            
+            // Set the appropriate bool based on player position
+            if (side > 0)
+            {
+                _animator.SetBool("OpenedIn", true);
+                _animator.SetBool("OpenedOut", false);
+            }
+            else
+            {
+                _animator.SetBool("OpenedIn", false);
+                _animator.SetBool("OpenedOut", true);
+            }
         }
     }
 }
