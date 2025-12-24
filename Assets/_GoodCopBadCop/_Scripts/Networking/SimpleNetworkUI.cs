7c756cd45db0cf31b69d01eb1156f4280c52b1f3
jason-ario <nosajtevol@gmail.com>
2025-12-24 04:51:08 +0400

    ADD - Network fix

--- Assets/_GoodCopBadCop/_Scripts/Networking/SimpleNetworkUI.cs
+++ Assets/_GoodCopBadCop/_Scripts/Networking/SimpleNetworkUI.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using System.Net.Sockets;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using UnityEngine;
@@ -6,12 +8,20 @@ public class SimpleNetworkUI : MonoBehaviour
 {
     public GameObject uiRoot;
     public string hostIP = "178.134.251.97"; // <-- HOST LAN IP
+    [SerializeField] UnityTransport transport;
+    [SerializeField] string ipAddress;
 
+    
     void Start()
     {
         NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+        ipAddress = "0.0.0.0";
+        SetIpAddress(); // Set the Ip to the above address
     }
 
+
+
+    
     void OnDestroy()
     {
         if (NetworkManager.Singleton == null) return;
@@ -21,7 +31,13 @@ public class SimpleNetworkUI : MonoBehaviour
     public void Host()
     {
         NetworkManager.Singleton.StartHost();
+        GetLocalIPAddress();
         uiRoot.SetActive(false);
+    }
+    
+    // To Join a game
+    public void StartClient() {
+        NetworkManager.Singleton.StartClient();
     }
 
     public void Join()
@@ -29,10 +45,35 @@ public class SimpleNetworkUI : MonoBehaviour
         var transport = (UnityTransport)
             NetworkManager.Singleton.NetworkConfig.NetworkTransport;
 
+        SetIpAddress();
         transport.ConnectionData.Address = hostIP;
 
         NetworkManager.Singleton.StartClient();
         uiRoot.SetActive(false);
+    }
+    
+    /* Gets the Ip Address of your connected network and
+    shows on the screen in order to let other players join
+    by inputing that Ip in the input field */
+    // ONLY FOR HOST SIDE 
+    public string GetLocalIPAddress() {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList) {
+            if (ip.AddressFamily == AddressFamily.InterNetwork) {
+                ipAddress = ip.ToString();
+                Debug.Log(ipAddress);
+                return ip.ToString();
+            }
+        }
+        throw new System.Exception("No network adapters with an IPv4 address in the system!");
+    }
+
+    /* Sets the Ip Address of the Connection Data in Unity Transport
+    to the Ip Address which was input in the Input Field */
+    // ONLY FOR CLIENT SIDE
+    public void SetIpAddress() {
+        transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        transport.ConnectionData.Address = ipAddress;
     }
 
     void OnClientConnected(ulong clientId)
