368902ed63dd9ac4088362a5d8d62765ac085d95
jason-ario <nosajtevol@gmail.com>
2026-01-02 01:21:08 +0400

    ADD - Button press

--- Assets/_GoodCopBadCop/_Scripts/Interactables/SwitchButton.cs
+++ Assets/_GoodCopBadCop/_Scripts/Interactables/SwitchButton.cs
@@ -1,9 +1,24 @@
+using Unity.Netcode;
 using UnityEngine;
 
 public class SwitchButton : Interactable
 {
+    [SerializeField] private AudioSource buttonPressSound;
+    [SerializeField] private Animator anim;
     public override void Interact(PlayerInteractionController player)
     {
         GameManager.Instance.TryStartLevel();
+        PlayButtonSoundClientRpc();
+    }
+
+    [ClientRpc]
+    private void PlayButtonSoundClientRpc()
+    {
+        if (buttonPressSound != null)
+        {
+            buttonPressSound.Play();
+        }
+        
+        anim.SetTrigger("Push");
     }
 }
