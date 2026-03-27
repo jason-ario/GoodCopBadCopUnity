a0accb536a9360f4eeff4f0e4fd2dfb6d896698a
jason-ario <nosajtevol@gmail.com>
2026-03-27 21:49:16 +0400

    ADD - turn off light

--- Assets/_GoodCopBadCop/_Scripts/Player/PlayerInstance.cs
+++ Assets/_GoodCopBadCop/_Scripts/Player/PlayerInstance.cs
@@ -7,6 +7,9 @@ public class PlayerInstance : NetworkBehaviour
     public static PlayerInstance Instance;
 
     [SerializeField] private ScreenDamage screenDamage;
+    [SerializeField] private bool isOutside;
+    public bool IsOutside => isOutside;
+    [SerializeField] private GameObject playerLight;
     
     public bool CanControl
     {
@@ -20,6 +23,12 @@ public class PlayerInstance : NetworkBehaviour
     {
         _playerMovementController = GetComponent<PlayerMovementController>();
         _playerInteractionController = GetComponent<PlayerInteractionController>();
+    }
+
+    public void SetIsOutside(bool value)
+    {
+        isOutside = value;
+        playerLight.SetActive(value);
     }
 
     public override void OnNetworkSpawn()
