b6aae5fb7bfbc56d018af33c347c37b8b4659cd8
jason-ario <nosajtevol@gmail.com>
2026-04-26 20:14:53 +0400

    ADD - power outage

--- Assets/_GoodCopBadCop/_Scripts/Environment/ElectricityController.cs
+++ Assets/_GoodCopBadCop/_Scripts/Environment/ElectricityController.cs
@@ -1,19 +1,31 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class ElectricityController : MonoBehaviour
 {
     [SerializeField] ElectricObject[] electricObjects;
+    [SerializeField] private AudioClip powerOffSound;
+    [SerializeField] private AudioClip powerOnSound;
+    [SerializeField] AudioSource sfxSource;
 
     [ContextMenu("Power Off")]
     public void PowerOff()
     {
+        StartCoroutine(PowerOffCoroutine());
+    }
+
+    IEnumerator PowerOffCoroutine()
+    {
+        sfxSource.PlayOneShot(powerOffSound);
+
+        yield return new WaitForSeconds(2f);
         foreach (var electricObject in electricObjects)
         {
             electricObject.OnElectricityTurnOff?.Invoke();
         }
+        
     }
-
     [ContextMenu("Power On")]
     public void PowerOn()
     {
@@ -21,5 +33,8 @@ public class ElectricityController : MonoBehaviour
         {
             electricObject.OnElectricityTurnOn?.Invoke();
         }
+        
+        sfxSource.PlayOneShot(powerOnSound);
+
     }
 }
