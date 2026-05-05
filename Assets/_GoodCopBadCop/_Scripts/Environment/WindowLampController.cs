324754957a764da4e78dfcbe79d09794ce0c3760
jason-ario <nosajtevol@gmail.com>
2026-05-05 20:09:07 +0400

    FIX - window lamp

--- Assets/_GoodCopBadCop/_Scripts/Environment/WindowLampController.cs
+++ Assets/_GoodCopBadCop/_Scripts/Environment/WindowLampController.cs
@@ -1,6 +1,7 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class WindowLampController : MonoBehaviour
+public class WindowLampController : NetworkBehaviour
 {
     [SerializeField] Light lightComponent;
     [SerializeField] Color greenLightColor;
@@ -8,16 +9,44 @@ public class WindowLampController : MonoBehaviour
     [SerializeField] Material greenMaterial;
     [SerializeField] Material redMaterial;
     [SerializeField] private MeshRenderer lightMeshRenderer;
-    
+
+    /// <summary>Turns the lamp green on all clients. Must be called from the server.</summary>
     [ContextMenu("Turn Green")]
     public void TurnGreen()
+    {
+        ApplyGreen();
+        if (IsSpawned)
+            TurnGreenClientRpc();
+    }
+
+    [ClientRpc]
+    private void TurnGreenClientRpc()
+    {
+        ApplyGreen();
+    }
+
+    /// <summary>Turns the lamp red on all clients. Must be called from the server.</summary>
+    [ContextMenu("Turn Red")]
+    public void TurnRed()
+    {
+        ApplyRed();
+        if (IsSpawned)
+            TurnRedClientRpc();
+    }
+
+    [ClientRpc]
+    private void TurnRedClientRpc()
+    {
+        ApplyRed();
+    }
+
+    private void ApplyGreen()
     {
         lightMeshRenderer.material = greenMaterial;
         lightComponent.color = greenLightColor;
     }
-    
-    [ContextMenu("Turn Red")]
-    public void TurnRed()
+
+    private void ApplyRed()
     {
         lightMeshRenderer.material = redMaterial;
         lightComponent.color = redLightColor;
