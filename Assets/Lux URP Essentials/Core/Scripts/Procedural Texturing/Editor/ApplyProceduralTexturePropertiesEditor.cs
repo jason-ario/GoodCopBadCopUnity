d5d5e2af861577e1982965bca513b1920294478a
jason-ario <nosajtevol@gmail.com>
2026-06-07 21:33:21 +0400

    ADD - grass

---
+++ Assets/Lux URP Essentials/Core/Scripts/Procedural Texturing/Editor/ApplyProceduralTexturePropertiesEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace LuxURPEssentials
+{
+    [CustomEditor(typeof(ApplyProceduralTextureProperties))]
+    public class ApplyProceduralTexturePropertiesEditor : Editor {
+        public override void OnInspectorGUI() {
+        	DrawDefaultInspector();
+
+        	ApplyProceduralTextureProperties script = (ApplyProceduralTextureProperties)target;
+
+        	if(GUILayout.Button("Apply")) {
+        		script.SyncMatWithProceduralTextureAsset();
+        	}
+        }
+    }
+}
