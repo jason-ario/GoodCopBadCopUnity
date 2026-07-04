46dcf764221d7f46d907655d0a44a007132f437d
Alex Hvesuk <44973765+hvesuk@users.noreply.github.com>
2026-07-05 01:42:47 +0400

    Fix settings menu tabs

--- Assets/_GoodCopBadCop/_Scripts/UI/Settings Menu/SelectableTab.cs
+++ Assets/_GoodCopBadCop/_Scripts/UI/Settings Menu/SelectableTab.cs
@@ -2,48 +2,98 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class SelectableTab : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
+public class SelectableTab : MonoBehaviour, IPointerDownHandler, IPointerClickHandler
 {
+    private static readonly Color SelectedBackgroundColor = Color.white;
+    private static readonly Color DeselectedBackgroundColor = Color.black;
+    private static readonly Color SelectedTextColor = Color.black;
+    private static readonly Color DeselectedTextColor = Color.white;
+
     private bool isSelected = false;
+    private Image backgroundImage;
+    private TextMeshProUGUI labelText;
+
     public bool IsSelected => isSelected;
-    [SerializeField] private Animator _animator;
     [SerializeField] UnityEvent onSelected;
-    
-    public void Select()
+
+    private void Awake()
     {
-        onSelected?.Invoke();
+        CacheStyleTargets();
+        ApplySelectedStyle(isSelected);
     }
 
-    public void SetHovering(bool hovering)
+    public void Select()
     {
-        if (isSelected)
+        SettingsMenu settingsMenu = GetComponentInParent<SettingsMenu>();
+        if (settingsMenu != null && settingsMenu.OpenSettingsForTab(this))
         {
             return;
         }
 
-        _animator.SetBool("Hovering", hovering);
+        onSelected?.Invoke();
     }
 
     public void SetSelected(bool selected)
     {
         isSelected = selected;
-        _animator.SetBool("Selected", selected);
-        SetHovering(false);
-    }
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        SetHovering(true);
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        SetHovering(false);
+        ApplySelectedStyle(selected);
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         Select();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Select();
+    }
+
+    private void ApplySelectedStyle(bool selected)
+    {
+        CacheStyleTargets();
+
+        if (backgroundImage != null)
+        {
+            backgroundImage.color = selected ? SelectedBackgroundColor : DeselectedBackgroundColor;
+        }
+
+        if (labelText != null)
+        {
+            labelText.color = selected ? SelectedTextColor : DeselectedTextColor;
+        }
+    }
+
+    private void CacheStyleTargets()
+    {
+        if (backgroundImage == null)
+        {
+            Transform background = transform.Find("BG");
+            if (background != null)
+            {
+                backgroundImage = background.GetComponent<Image>();
+            }
+        }
+
+        if (labelText != null)
+        {
+            return;
+        }
+
+        TextMeshProUGUI[] textComponents = GetComponentsInChildren<TextMeshProUGUI>(true);
+        foreach (TextMeshProUGUI textComponent in textComponents)
+        {
+            textComponent.raycastTarget = false;
+
+            if (textComponent.text == ">")
+            {
+                continue;
+            }
+
+            labelText = textComponent;
+            break;
+        }
     }
 }
--- Assets/_GoodCopBadCop/_Scripts/UI/Settings Menu/SettingsMenu.cs
+++ Assets/_GoodCopBadCop/_Scripts/UI/Settings Menu/SettingsMenu.cs
@@ -1,5 +1,5 @@
-using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
@@ -9,46 +9,187 @@ public class SettingsMenu : MonoBehaviour
     [SerializeField] private GameObject controlsSettings;
     [SerializeField] SelectableTab[] selectableTabs;
 
+    private void Awake()
+    {
+        DisableDecorativeRaycastTargets();
+    }
+
     private void OnEnable()
     {
-        OpenGameplaySettings();
+        OpenDefaultSettings();
     }
 
     public void OpenGameplaySettings()
     {
-        foreach (var selectableTab in selectableTabs)
-        {
-            selectableTab.SetSelected(false);
-        }
-        
-        selectableTabs[0].SetSelected(true);
-        gameplaySettings.SetActive(true);
-        graphicsSettings.SetActive(false);
-        audioSettings.SetActive(false);
-        controlsSettings.SetActive(false);
+        OpenSettingsPanel(gameplaySettings, 0);
     }
     
     public void OpenGraphicsSettings()
     {
-        gameplaySettings.SetActive(false);
-        graphicsSettings.SetActive(true);
-        audioSettings.SetActive(false);
-        controlsSettings.SetActive(false);
+        OpenSettingsPanel(graphicsSettings, 1);
     }
     
     public void OpenAudioSettings()
     {
-        gameplaySettings.SetActive(false);
-        graphicsSettings.SetActive(false);
-        audioSettings.SetActive(true);
-        controlsSettings.SetActive(false);
+        OpenSettingsPanel(audioSettings, 2);
     }
     
     public void OpenControlSettings()
     {
-        gameplaySettings.SetActive(false);
-        graphicsSettings.SetActive(false);
-        audioSettings.SetActive(false);
-        controlsSettings.SetActive(true);
+        OpenSettingsPanel(controlsSettings, 3);
+    }
+
+    public bool OpenSettingsForTab(SelectableTab selectableTab)
+    {
+        for (int i = 0; i < selectableTabs.Length; i++)
+        {
+            if (selectableTabs[i] != selectableTab)
+            {
+                continue;
+            }
+
+            OpenSettingsByIndex(i);
+            return true;
+        }
+
+        return false;
+    }
+
+    private void OpenDefaultSettings()
+    {
+        if (HasVisibleSettings(gameplaySettings))
+        {
+            OpenSettingsPanel(gameplaySettings, 0);
+            return;
+        }
+
+        if (HasVisibleSettings(graphicsSettings))
+        {
+            OpenSettingsPanel(graphicsSettings, 1);
+            return;
+        }
+
+        if (HasVisibleSettings(audioSettings))
+        {
+            OpenSettingsPanel(audioSettings, 2);
+            return;
+        }
+
+        OpenSettingsPanel(controlsSettings, 3);
+    }
+
+    private void OpenSettingsByIndex(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                OpenGameplaySettings();
+                break;
+            case 1:
+                OpenGraphicsSettings();
+                break;
+            case 2:
+                OpenAudioSettings();
+                break;
+            case 3:
+                OpenControlSettings();
+                break;
+        }
+    }
+
+    private void OpenSettingsPanel(GameObject targetPanel, int selectedTabIndex)
+    {
+        gameplaySettings.SetActive(targetPanel == gameplaySettings);
+        graphicsSettings.SetActive(targetPanel == graphicsSettings);
+        audioSettings.SetActive(targetPanel == audioSettings);
+        controlsSettings.SetActive(targetPanel == controlsSettings);
+
+        for (int i = 0; i < selectableTabs.Length; i++)
+        {
+            selectableTabs[i].SetSelected(i == selectedTabIndex);
+        }
+
+        RebuildPanelLayout(targetPanel);
+    }
+
+    private static bool HasVisibleSettings(GameObject settingsPanel)
+    {
+        if (settingsPanel == null)
+        {
+            return false;
+        }
+
+        ScrollRect scrollRect = settingsPanel.GetComponent<ScrollRect>();
+        Transform content = scrollRect != null && scrollRect.content != null
+            ? scrollRect.content
+            : settingsPanel.transform;
+
+        for (int i = 0; i < content.childCount; i++)
+        {
+            if (content.GetChild(i).gameObject.activeSelf)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static void RebuildPanelLayout(GameObject settingsPanel)
+    {
+        if (settingsPanel == null)
+        {
+            return;
+        }
+
+        Canvas.ForceUpdateCanvases();
+
+        RectTransform[] rectTransforms = settingsPanel.GetComponentsInChildren<RectTransform>(true);
+        foreach (RectTransform rectTransform in rectTransforms)
+        {
+            LayoutRebuilder.ForceRebuildLayoutImmediate(rectTransform);
+        }
+
+        Canvas.ForceUpdateCanvases();
+    }
+
+    private void DisableDecorativeRaycastTargets()
+    {
+        DisableImageRaycastTarget(gameObject);
+        DisableImageRaycastTarget(transform.Find("Settings Side Bar"));
+        DisableImageRaycastTarget(transform.Find("Settings Main View"));
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+            if (child.name.StartsWith("Line"))
+            {
+                DisableImageRaycastTarget(child);
+            }
+        }
+    }
+
+    private static void DisableImageRaycastTarget(Transform target)
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        DisableImageRaycastTarget(target.gameObject);
+    }
+
+    private static void DisableImageRaycastTarget(GameObject target)
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        Image image = target.GetComponent<Image>();
+        if (image != null)
+        {
+            image.raycastTarget = false;
+        }
     }
 }
