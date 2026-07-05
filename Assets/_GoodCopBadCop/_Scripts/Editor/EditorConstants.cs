63b6cc757965cfb4583fce03d2626d09dfc236ba
Alex Hvesuk <44973765+hvesuk@users.noreply.github.com>
2026-07-05 06:39:58 +0400

    Remove underscore from editor menu

--- Assets/_GoodCopBadCop/_Scripts/Editor/EditorConstants.cs
+++ Assets/_GoodCopBadCop/_Scripts/Editor/EditorConstants.cs
@@ -1,6 +1,6 @@
 public static class EditorConstants
 {
-    public const string RootMenuPath = "Tools/_GoodCopBadCop/";
+    public const string RootMenuPath = "Tools/GoodCopBadCop/";
     public const int RootMenuPriority = -1000;
 
     public const string EnvironmentDebugMenuPath = RootMenuPath + "Environment Debug";
