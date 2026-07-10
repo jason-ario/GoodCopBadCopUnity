23cbb0bd53da60c4e626fda070d928574085cd66
jason-ario <nosajtevol@gmail.com>
2026-07-11 00:40:39 +0400

    ADD - farm models pack

---
+++ Assets/ALP_Assets/Farm Models Pack/Models/Farm_windmill01/Scripts/WindmillRotation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class WindmillRotation : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_Blades;
+     [SerializeField]
+    private Vector3 m_BladesSpeed;
+
+    [Space]
+
+    [SerializeField]
+    private Transform m_Support;
+
+    [SerializeField]
+    private Vector3 m_SupportRotationAmount;
+
+    [SerializeField]
+    private float m_SupportRotationSpeed = 1f;
+
+
+    private void Update()
+    {
+        if(m_Blades != null)
+        {
+            m_Blades.localRotation *= Quaternion.Euler(m_BladesSpeed * Time.deltaTime);
+            m_Support.localRotation = Quaternion.Euler(m_SupportRotationAmount * Mathf.Cos(Time.time * m_SupportRotationSpeed));
+        }
+    }
+}
